37a80c1a2b662df57b49dcac1cbbe8cca5442307
shijayas <anjithashijay@gmail.com>
2025-06-20 23:37:03 +0530

    changed webRtc hook

--- server/src/presentation/middleware/corsMiddleware.ts
+++ server/src/presentation/middleware/corsMiddleware.ts
@@ -1,11 +1,11 @@
-import cors from 'cors';
+// import cors from 'cors';
 
-const corsMiddleware = cors({
-  origin: 'https://social-app-ten-nu.vercel.app', // ✅ only this
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-  optionsSuccessStatus: 204,
-});
+// const corsMiddleware = cors({
+//   origin: 'https://social-app-ten-nu.vercel.app', // ✅ only this
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+//   optionsSuccessStatus: 204,
+// });
 
-export default corsMiddleware;
+// export default corsMiddleware;
