9c103f0fc6f3a3516d0c5c9f4560f3249a3b9948
shijayas <anjithashijay@gmail.com>
2025-07-26 07:04:33 +0530

    webRtc

--- client/src/hooks/webrtc/useWebRTC.ts
+++ client/src/hooks/webrtc/useWebRTC.ts
@@ -59,7 +59,7 @@ const pc = new RTCPeerConnection({
   iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
     {
-      urls: ['turn:13.202.177.174:3478?transport=udp'], 
+      urls: ['turn:13.202.177.174:3478'], 
       username: 'myuser',
       credential: 'mypassword',
     },
