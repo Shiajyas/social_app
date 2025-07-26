bd6ebd2d86d91e739e9af59afb32e9b9f4cb1c1a
shijayas <anjithashijay@gmail.com>
2025-07-26 06:28:34 +0530

    change spam report socket

--- client/src/ customComponents/adminDashbord/spamManagement/Spam.tsx
+++ client/src/ customComponents/adminDashbord/spamManagement/Spam.tsx
@@ -122,7 +122,7 @@ export default function Spam() {
     return () => {
       socket.off('admin:newReport', onNewReport);
     };
-  }, [qc,  incRep]);
+  }, [ ]);
 
   const toggleSelect = (id: string) => {
     const s = new Set(selected);
